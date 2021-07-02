33c5754053dcd9efcb43f02d60781d576bf2ec04
Arturo Sánchez <arturo9806@hotmail.com>
2021-07-02 00:13:57 -0500

    prueba

--- figurasGeometricas.js
+++ figurasGeometricas.js
@@ -26,23 +26,27 @@ let circuloPerimetro = (radio) => {
     return (pi * 2) * r;
 }
 
-let areaCirculo = circuloArea(5)
-let perimetroCirculo = circuloPerimetro(5);
-let area = cuadradoArea(5);
-let perimetro = cuadradoPerimetro(5);
-let areaTriangulo = trianguloArea(5, 6);
-let perimetroTriangulo = trianguloPerimetro(5, 6, 7);
-console.log("el resultado del cuadrado es area: ", area +" perimetro: ", perimetro);
-console.log("el resultado del circulo es area: ", areaCirculo +" perimetro: ", perimetroCirculo);
-console.log("el resultado del Triangulo es area: ", areaTriangulo +" perimetro: ", perimetroTriangulo);
 
 //interaccion con HTML5 
 
 function calcularAreaCuadrado () {
     //Obtenemos el valor del input
-    const ladoInput = document.getElementById("inputLado")
+    const input = document.getElementById("inputLado")
     //convertimos el argumento en valor 
-    const ladoValue = ladoInput.value;
-    const areaCuadrado = cuadradoArea(ladoValue);
+
+    const value = input.value;
+    const areaCuadrado = cuadradoArea(value);
     alert(areaCuadrado);
+
+}
+
+function calcularPerimetroCuadrado () {
+    //Obtenemos el valor del input
+    const input = document.getElementById("inputLado")
+    //convertimos el argumento en valor 
+
+    const value = input.value;
+    const perimetroCuadrado = cuadradoPerimetro(value);
+    alert(perimetroCuadrado);
+    
 }
